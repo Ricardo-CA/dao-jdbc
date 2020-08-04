77d7d5868b6b5e204419c267ddea152d59b2bb30
Ricardo Carvalho Agostinho <ricorino1@gmail.com>
2020-08-04 18:45:24 -0300

    insert implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
@@ -9,6 +10,9 @@ import model.entities.Department;
 public class Program2 {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 		Department department = departmentDao.findById(2);
 		System.out.println("=========TEST 1 ============ findById======= " + department);
@@ -17,6 +21,13 @@ public class Program2 {
 		System.out.println("=======TEST 2 ======= findAll========");
 		list.forEach(System.out :: println);
 		
+		
+		System.out.println("=========TEST 3==========insert======");
+		Department newdepartment = new Department("Cosmetics", null);
+		departmentDao.insert(newdepartment);
+		System.out.println("insertion test completed");
+
+		sc.close();
 
 	}
 
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -4,16 +4,18 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
 import db.DB;
 import db.DbException;
+import db.DbIntegrityException;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
-public class DepartmentDaoJDBC implements DepartmentDao{
-	
+public class DepartmentDaoJDBC implements DepartmentDao {
+
 	private Connection conn;
 
 	public DepartmentDaoJDBC(Connection conn) {
@@ -22,36 +24,54 @@ public class DepartmentDaoJDBC implements DepartmentDao{
 
 	@Override
 	public void insert(Department obj) {
-		// TODO Auto-generated method stub
-		
+		PreparedStatement st = null;
+
+		try {
+			st = conn.prepareStatement("insert into department (Name) " + "values   (?)",
+					Statement.RETURN_GENERATED_KEYS);
+			st.setString(1, obj.getName());
+			int rowsAffected = st.executeUpdate();
+
+			if (rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				if (rs.next()) {
+					int id = rs.getInt(1);
+					obj.setId(id);
+					DB.closeResultSet(rs);
+				}
+			} else {
+				throw new DbException("Unexpected error! no rows affected");
+			}
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+		}
+
 	}
 
 	@Override
 	public void update(Department obj) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public Department findById(Integer id) {
 		PreparedStatement st = null;
 		ResultSet rs = null;
-		
+
 		try {
-			st = conn.prepareStatement(
-					"select * from department "
-					+ "where department.Id = ? ");
-			
+			st = conn.prepareStatement("select * from department " + "where department.Id = ? ");
+
 			st.setInt(1, id);
 			rs = st.executeQuery();
-		
-			if(rs.next()) {
+
+			if (rs.next()) {
 				Department obj = new Department();
 				obj.setId(rs.getInt("Id"));
 				obj.setName(rs.getString("Name"));
@@ -70,14 +90,13 @@ public class DepartmentDaoJDBC implements DepartmentDao{
 	public List<Department> findAll() {
 		PreparedStatement st = null;
 		ResultSet rs = null;
-		
+
 		try {
-			st = conn.prepareStatement(
-					"select * from department order by Name");
-			
+			st = conn.prepareStatement("select * from department order by Name");
+
 			rs = st.executeQuery();
 			List<Department> list = new ArrayList<>();
-			while(rs.next()) {
+			while (rs.next()) {
 				Department obj = new Department();
 				obj.setId(rs.getInt("Id"));
 				obj.setName(rs.getString("Name"));
